642a26b7f4fb29943197c0659a2256edf73e2d9d
Melinda Tran <tranmelinda84@yahoo.com>
2021-02-15 23:47:21 -0800

    Menu Button won't work

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 var isPlaying = false;
-var speed = 0;
-var combo = 0;
 var maxCombo = 0;
 var score = 0;
+var animation = 'moveDown';
+var keyContainer;
+var songContainer;
+var keyPress;
 
-var keyPress = {
+var keyHolding = {
     a: false,
     w: false,
     d: false,
@@ -13,7 +15,9 @@ var keyPress = {
     l: false
 };
 
-var keyHits = {
+var keyHits = { perfect: 0, great: 0, good: 0, bad: 0, miss: 0 };
+
+var keyMultiple = {
     perfect: 2,
     great: 1,
     good: 0.5,
@@ -24,10 +28,82 @@ var keyHits = {
 
 var menuButton = function () {
     var menuButton = document.querySelector('.btn--start');
-    menuButton.addEventListener('click', function () {
-        document.querySelector('.instructions').style.opacity = 0;
-        canvas = document.getElementById('canvas'),
-        ctx = canvas.getContext('2d'),
-        document.querySelector('.menu').classList.add(show);
+    menuButton.removeEventListener('click', function () {
+        isPlaying = true;
+        displayResults();
+        document.querySelector('.menu').style.opacity = 0;
     })
+}
+
+var keySetUp = function () {
+    var keyIndex;
+    document.addEventListener("keydown", function (event) {
+        switch (event.key) {
+            case "KeyA":
+                keyHolding.a = true;
+                keyIndex = 0;
+                break;
+            case "KeyW":
+                keyHolding.w = true;
+                keyIndex = 1;
+                break;
+            case "KeyD":
+                keyHolding.d = true;
+                keyIndex = 2;
+                break;
+            case "KeyJ":
+                keyHolding.j = true;
+                keyIndex = 3;
+                break;
+            case "KeyI":
+                keyHolding.i = true;
+                keyIndex = 4;
+                break;
+            case "KeyL":
+                keyHolding.l = true;
+                keyIndex = 5;
+                break;
+        }
+        //keyPress[keyIndex].style.display = 'block';
+    }, false);
+
+    document.addEventListener("keyup", function (event) {
+        switch (event.key) {
+            case "KeyA":
+                keyHolding.a = false;
+                keyIndex = 0;
+                break;
+            case "KeyW":
+                keyHolding.w = false;
+                keyIndex = 1;
+                break;
+            case "KeyD":
+                keyHolding.d = false;
+                keyIndex = 2;
+                break;
+            case "KeyJ":
+                keyHolding.j = false;
+                keyIndex = 3;
+                break;
+            case "KeyI":
+                keyHolding.i = false;
+                keyIndex = 4;
+                break;
+            case "KeyL":
+                keyHolding.l = false;
+                keyIndex = 5;
+                break;
+        }
+        //keyPress[keyIndex].style.display = 'none';
+    }, false);
+}
+
+var displayResults = function () {
+    document.querySelector('.perfect_total').innerHTML = keyHits.perfect;
+    document.querySelector('.great_total').innerHTML = keyHits.great;
+    document.querySelector('.good_total').innerHTML = keyHits.good;
+    document.querySelector('.bad_total').innerHTML = keyHits.bad;
+    document.querySelector('.miss_total').innerHTML = keyHits.miss;
+    document.querySelector('.combo_total').innerHTML = maxCombo;
+    document.querySelector('.score_total').innerHTML = score;
 }
